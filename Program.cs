572e26d931779be43695e7639f2b7ca3c3682d44
lucabulla34 <luca.bulla.7e9@itb.cat>
2025-11-27 10:41:06 +0100

    Exercise one completed. Closes #1

--- Program.cs
+++ Program.cs
@@ -4,6 +4,44 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        const string MsgInput = "Input a number to check if it's between the range [10, 50]: ";
+        const string MsgError = "Invalid input.";
+        const string MsgRangeTrue = "Number {0} is between the range [10, 50].";
+        const string MsgRangeFalse = "Number {0} is NOT between the range [10, 50].";
+
+        int input;
+        bool isInput, isRange;
+
+        do
+        {
+            Console.WriteLine(MsgInput);
+            isInput = Int32.TryParse(Console.ReadLine(), out input);
+            if (!isInput)
+            {
+                Console.WriteLine(MsgError);
+            }
+        } while (!isInput);
+        isRange = NumberInRange(input);
+        if (isRange)
+        {
+            Console.WriteLine(MsgRangeTrue, input);
+        }
+        else Console.WriteLine(MsgRangeFalse, input);
+    }
+
+
+    public static bool NumberInRange(int value)
+    {
+        int lowerEnd = 10, higherEnd = 50;
+        bool isRange;
+
+
+        if (value >= lowerEnd && value <= higherEnd)
+        {
+            isRange = true;
+        }
+        else isRange = false;
+
+        return isRange;
     }
 }
